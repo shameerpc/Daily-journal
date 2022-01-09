6bb6cb120a0a4fe395aa298539bb01d5d26b0ad9
shameerpc <keralatechvlog@gmail.com>
2022-01-09 19:32:14 +0530

    complete the daily journal using ejs and nodejs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _=require("lodash");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,17 +13,20 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-let posts=[];
+let posts=[];  
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+"/public"));
 
 app.get("/",function(req,res){
+  
+   
+  
   res.render("home",{homeContent:homeStartingContent,homeArray:posts});
  
 })
 app.get("/about",function(req,res){
-  res.render("about",{aboutParagraph:aboutContent})
+  res.render("about",{aboutParagraph:aboutContent})                                                            
 })
 app.get("/contact",function(req,res){
   res.render("contact",{contactParagraph:contactContent})
@@ -34,14 +39,27 @@ app.post("/compose",function(req,res){
      const post={
        title:req.body.postTitle,
        body:req.body.postBody
+
        }
+       
        posts.push(post);
+      
      
        res.redirect("/");
      
 })
 app.get("/posts/:title",function(req,res){
-  console.log(req.params.title)
+   const reqTitle = _.lowerCase(req.params.title)
+   posts.forEach(function(post){
+     const title=_.lowerCase(post.title)
+     const bodyTag=post.body;
+
+     if(reqTitle===title){
+       console.log("matched title!");
+       res.render("post",{postTitle:post.title,bodyPart:post.body})
+      
+     }
+   })
 })
 
 
